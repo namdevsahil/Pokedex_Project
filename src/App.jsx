721ff9c30b8b87db5c96015963280775da7ac66e
namdevsahil <sahilnamdev1213@gmail.com>
2024-05-24 11:08:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Pokedex from './Components/Pokedex/Pokedex'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+   <Pokedex/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/PokemonList/PokemonList.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import './PokemonList.css';
+import Pokemon from "../Pokemon/Pokemon";
+
+function PokemonList(){
+
+    const [ pokemonList, setPokemonList ] = useState([]);
+    const [isLoading, setIsLoading ] = useState(true);
+
+    const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+    async function downloadPokemons(){
+        const response = await axios.get(POKEDEX_URL);     //this download list
+
+        const pokemonResults = response.data.results;    //we get the array of pokemon 
+        console.log(response.data);
+
+        // iterating over the array of pokemon ,and useing their url, to create an array of prommise
+        // that will downloaded 20 pokemons
+        const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
+        // passing that promises array to axios.all
+        const pokemonData = await axios.all(pokemonResultPromise);
+        console.log(pokemonData);
+         const res = pokemonData.map((pokeData) => {
+            const pokemon = pokeData.data;
+            return {
+                id: pokemon.id,
+                name: pokemon.name, 
+                image: (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default: pokemon.sprites.front.dafault,
+                types: pokemon.types
+        };
+        });
+        console.log(res);
+        setPokemonList(res);
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                downloadPokemons();
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        
+        fetchData();
+    }, []);
+
+    return (
+        <div className="pokemon-list-wrapper">
+            <div>Pokemon List</div>
+            {(isLoading) ? 'Loading....' : pokemonList.map((p)=> <Pokemon name={p.name} image={p.image} key={p.id}/>)}
+
+        </div>
+    )
+}
+
+export default PokemonList;
